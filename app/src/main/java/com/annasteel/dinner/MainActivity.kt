9f864152736783085bbbed7c542a1f64069a3ee0
piladzisi <piladzisi@gmail.com>
2019-06-10 12:42:15 +0300

    functional

--- app/src/main/java/com/annasteel/dinner/MainActivity.kt
+++ app/src/main/java/com/annasteel/dinner/MainActivity.kt
@@ -2,11 +2,29 @@ package com.annasteel.dinner
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+val foodList = arrayListOf<String>("Chinese", "Sushi", "Pizza", "Pasta", "Frozen Veggies", "Fish", "Bakery" )
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodtxt.text.toString()
+            foodList.add(newFood)
+            addFoodtxt.text.clear()
+        }
+
     }
 }
